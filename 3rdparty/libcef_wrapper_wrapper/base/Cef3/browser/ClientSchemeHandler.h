090758a5bac377e412639f115b76bc5b3bff801c
jiaYF <jiajiajiajia10003@163.com>
2019-04-29 17:17:04 +0800

    Dr.Wise_Client_CTA版本：添加交互代码。发送数据，通过json字符串交互。

--- 3rdparty/libcef_wrapper_wrapper/base/Cef3/browser/ClientSchemeHandler.h
+++ 3rdparty/libcef_wrapper_wrapper/base/Cef3/browser/ClientSchemeHandler.h
@@ -12,9 +12,14 @@
 #include "include/cef_scheme.h"
 #include "include/wrapper/cef_helpers.h"
 
+#include "json/json.h"
+
 #define HANDLER_SCHEME_NAME "client"
 #define HANDLER_DOMAIN_NAME "resources"
 #define HANDLER_POSTDATA_NAME "postdata"
+
+#define JSON_KEY_NAME_1 "func_name"
+#define JSON_KEY_NAME_2 "paras_name"
 
 // Implementation of the schema handler for client:// requests.
 class ClientSchemeHandler : public CefResourceHandler {
@@ -49,61 +54,47 @@ public:
 							if (arraybuffer) {
 								memset(arraybuffer, 0, length);
 								data->GetBytes(length, arraybuffer);
-							}							
-							//buffer里存储了arraybuffer的数据
-							wchar_t *wc_buffer = new wchar_t[(length + 2) / 2];
 
-							if (wc_buffer) {
-								memset(wc_buffer, 0, length + 2);
-								memcpy((char*)wc_buffer, arraybuffer, length);
-								std::wstring wsPostData2(wc_buffer);
-								//::MessageBox(NULL, wsPostData2.c_str(), _T("Postdata"), MB_OK);
-								// 遇到有空格的文本时，会出现问题。
-								static int count = 1;
-								std::wstring text;
-								std::wstringstream wss;
-								wss << _T("jsSendCount('");
-								wss << count;
-								//wss << _T("");
-								wss << _T(".send_txt:");
-								wss << wsPostData2;
+								// 解析从浏览器发送过来的Json数据
+								Json::Reader reader;
+								Json::Value root;
+								bool ret = reader.parse(arraybuffer, root, false);
+								if (!ret) {
+									return false;
+								}
+								// 获得关键性的参数
+								std::string key_name1("");
+								std::string key_name2("");
+								if (root[JSON_KEY_NAME_1].isString()) {
+									key_name1 = root[JSON_KEY_NAME_1].asString();
+								}
+								if (root[JSON_KEY_NAME_2].isString()) {
+									key_name2 = root[JSON_KEY_NAME_2].asString();
+								}
 
-								wss << _T("')");
-								wss >> text;
+								// 模拟再发送给浏览器
+								Json::FastWriter writer;
+								Json::Value inputjson;
+								inputjson[JSON_KEY_NAME_1] = key_name1;
+								inputjson[JSON_KEY_NAME_2] = key_name2;
+
+								std::string jsonstr = writer.write(inputjson);
+
+								// 有换行符的json字符串， JS不能处理。
+								if (*jsonstr.rbegin() == '\n') {
+									jsonstr.erase(jsonstr.end() - 1);
+								}
 								
+								std::string text("jsSendCount('");
+								std::string postfix("')");
+								text += jsonstr;
+								text += postfix;
 								frame_->ExecuteJavaScript(text.c_str(), "", 0);
-								count++;
 							}
 						}
 					}
 				}
 			}
-
-
-			//// 转换为小写
-			//for (int i = 0; i < url.size(); i++)
-			//	url[i] = std::tolower(url[i]);
-
-			//// 解析相对路径，创建并返回CefStreamResourceHandler
-			//std::string baseUrl = HANDLER_SCHEME_NAME + "://" + HANDLER_DOMAIN_NAME;
-			//const std::string& relative_path = url.substr(baseUrl.length());
-			//if (!relative_path.empty()) {
-
-			//	TCHAR tszModule[MAX_PATH + 1] = { 0 };
-			//	::GetCurrentDirectory(MAX_PATH, tszModule);
-
-			//	CefString instancePath = tszModule;
-			//	instancePath.FromString(instancePath.ToString() + "/resources/" + relative_path);
-
-			//	CefRefPtr<CefStreamReader> stream =
-			//		CefStreamReader::CreateForFile(instancePath);
-
-			//	if (stream.get()) {
-			//		handler = new CefStreamResourceHandler(
-			//			request->mime_type_resolver().Run(url),
-			//			stream);
-			//	}
-			//}
 
 			if (handled) {
 				// Indicate the headers are available.
