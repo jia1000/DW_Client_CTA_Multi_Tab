ef73fe70779901b22b53f6ce4b8b62d325b95d8f
jiaYF <jiajiajiajia10003@163.com>
2019-10-24 17:06:20 +0800

    Dr.Wise_Client_CTA版本：Multi_Tab分支：调试使用tab1页签。dev_tools也使用tab1的。

--- BrowserApp/main/WndFrameBase.cpp
+++ BrowserApp/main/WndFrameBase.cpp
@@ -90,7 +90,9 @@ void CWndFrameBase::Notify(TNotifyUI& msg)
 			CefBrowserSettings settings;
 			windowInfo.width = 900;
 			windowInfo.height = 700;
-			m_pBrowserUI2->m_pBrowser->GetHost()->ShowDevTools(windowInfo, m_pBrowserUI2->m_pClientHandler, settings, CefPoint());
+			//m_pBrowserUI2->m_pBrowser->GetHost()->ShowDevTools(windowInfo, m_pBrowserUI2->m_pClientHandler, settings, CefPoint());
+			m_pBrowserUI->m_pBrowser->GetHost()->ShowDevTools(windowInfo, m_pBrowserUI->m_pClientHandler, settings, CefPoint());
+
 		}
 		else if (_tcscmp(pszCtrlName, _T("btn_stop")) == 0)
 		{			
